b20a5ca5272a8c750160ddb97c9ba3a26874a248
simple <simple.continue@gmail.com>
2018-06-26 16:53:40 +0800

    update special fee rate

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -2,6 +2,21 @@ class Balance < ApplicationRecord
   belongs_to :user, class_name: 'User', inverse_of: :balance
   validate :ensure_fee_rate_range
 
+  def fee_rate
+    now = Time.zone.now
+    start_time = "2018-06-30T00:00:00+08:00"
+    end_time = "2018-07-06T23:59:59+08:00"
+    special_fee_rate = 10
+    normal_fee_rate = super
+    special_fee_rate = [special_fee_rate, normal_fee_rate].min
+    special_fee_rate = [special_fee_rate, 10].max
+    if now >= start_time && now <= end_time
+      return  special_fee_rate
+    else
+      super
+    end
+  end
+
   def order_finalize!(order)
     if order.total > 0
       self.lock!    
